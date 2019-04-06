f857c2e9111cedbf589057c440741314efbfec93
Cyril Marchive <cyril.marchive@gmail.com>
2019-04-06 14:57:47 -0400

    Commit initial

---
+++ src/main/java/com/cmarchive/bank/serviceinteraction/ServiceInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.cmarchive.bank.serviceinteraction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceInteractionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceInteractionApplication.class, args);
+	}
+
+}
